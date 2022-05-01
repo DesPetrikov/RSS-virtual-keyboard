1bb0defcf6694105e383a01a1c276412f8e89018
DesPetrikov <desik1603@gmail.com>
2022-05-01 18:54:43 +0300

    init: initialized project with webpack-ts-eslint-prettier

---
+++ global.d.ts
@@ -0,0 +1,18 @@
+declare module '*.jpg' {
+  const value: any;
+  export = value;
+}
+
+declare module '*.png' {
+  const value: any;
+  export = value;
+}
+
+declare module '*.svg?inline' {
+  const content: any;
+  export default content;
+}
+declare module '*.svg' {
+  const content: any;
+  export default content;
+}
